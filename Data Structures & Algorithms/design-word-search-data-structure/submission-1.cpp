6305a6f3b5211b3077f54a3ff9c2110eccd97e7c
Kaustubh Rai <57839810+rai-kaustubh@users.noreply.github.com>
2026-09-04 22:15:25 +0530

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.cpp
@@ -0,0 +1,47 @@
+class TrieNode {
+   public:
+    unordered_map<char, TrieNode*> children;
+    bool endOfWord = false;
+};
+class WordDictionary {
+    TrieNode* root;
+
+   public:
+    WordDictionary() { this->root = new TrieNode(); }
+
+    void addWord(string word) {
+        auto curr = root;
+        for (char c : word) {
+            if (!curr->children.count(c)) {
+                curr->children[c] = new TrieNode();
+            }
+            curr = curr->children[c];
+        }
+
+        curr->endOfWord = true;
+    }
+
+    bool search(string word) { return dfs(0, root, word); }
+
+    bool dfs(int index, TrieNode* root, string word) {
+        TrieNode* curr = root;
+
+        for (int i = index; i < word.size(); i++) {
+            if (word[i] == '.') {
+                for (auto& [c, child] : curr->children) {
+                    if (dfs(i + 1, child, word)) {
+                        return true;
+                    }
+                }
+                return false;
+            } else {
+                if (!curr->children.count(word[i])) {
+                    return false;
+                }
+                curr = curr->children[word[i]];
+            }
+        }
+
+        return curr->endOfWord;
+    }
+};
