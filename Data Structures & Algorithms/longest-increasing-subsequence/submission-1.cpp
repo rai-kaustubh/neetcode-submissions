084b685c79d645a92fba81fd66be92a4fe886077
Kaustubh Rai <57839810+rai-kaustubh@users.noreply.github.com>
2026-09-09 18:46:19 +0530

    Add: longest-increasing-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    unordered_map<int, int> mp;
+    int lengthOfLIS(vector<int>& nums) {
+        int LIS = 1;
+        for(int j=0;j<nums.size();j++){
+            LIS = max(LIS, dfs(j, nums));
+        }
+
+        return LIS;
+    }
+
+    int dfs(int i, vector<int>& nums){
+        if(i==nums.size()) return 1;
+        if(mp.count(i)){
+            return mp[i];
+        }
+
+        int LIS = 1;
+        for(int j=i+1;j<nums.size();j++){
+            if(nums[j]>nums[i]){
+                LIS = max(LIS, 1+dfs(j, nums));
+            }
+        }
+        mp[i] = LIS;
+        return LIS;
+    }
+};
